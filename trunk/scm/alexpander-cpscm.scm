3359cdf66558c7c15352b14cd31c9a24288f5260
danmbox <danmbox@5a174e22-9223-0410-a059-a38d3f06340e>
2006-12-02 04:50:17 +0000

    Fixed copyright notice for alexpander.

--- trunk/scm/alexpander-cpscm.scm
+++ trunk/scm/alexpander-cpscm.scm
@@ -1,20 +1,7 @@
-;; Copyright (C) 2006 Dan Muresan
-;;
-;; This file is part of cpscm.
-;;
-;; cpscm is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 2 of the License, or
-;; (at your option) any later version.
-;;
-;; cpscm is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-;;
-;; You should have received a copy of the GNU General Public License
-;; along with cpscm; if not, write to the Free Software
-;; Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+;; Source: http://petrofsky.org/src/alexpander.scm
+;; Original author: Al Petrofsky
+;; Modified by Dan Muresan in 2006 for cpscm.
+;; Original notices follow.
 
 ;; alexpander.scm: a macro expander for scheme.
 ;; $Id: alexpander.scm,v 1.63 2006/05/28 03:42:52 al Exp $
