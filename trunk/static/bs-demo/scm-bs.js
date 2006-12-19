c47400ac78102357047db4b201e14ba16f0d8e85
danmbox <danmbox@5a174e22-9223-0410-a059-a38d3f06340e>
2006-12-19 13:04:55 +0000

    Added bubblesort examples, fixed JS error handling.

---
+++ trunk/static/bs-demo/scm-bs.js
@@ -0,0 +1,262 @@
+var cpscmbubble_2d_sort_21_ = function (__args) {
+var cpscm_20__25_gs_3a__3a_k163=__args.car; __args=__args.cdr;
+var cpscm_5f_n_5f_41=__args.car; __args=__args.cdr;
+var cpscm_5f_cmp_5f_42=__args.car; __args=__args.cdr;
+var cpscm_5f_cref_5f_43=__args.car; __args=__args.cdr;
+var cpscm_5f_cset_21__5f_44=__args.car; __args=__args.cdr;
+return new cpscm__Trampoline (function () {
+return (function (__args) {
+var cpscm_20__25_gs_3a__3a_k166=__args.car; __args=__args.cdr;
+var cpscm_5f_n_2d_1_5f_45=__args.car; __args=__args.cdr;
+var cpscm_5f_chk_5f_46=__args.car; __args=__args.cdr;
+var cpscm_5f_pass_5f_47=__args.car; __args=__args.cdr;
+return new cpscm__Trampoline (function () {
+return (cpscm_2d_) (cpscm__list (function (__args) {
+var cpscm_20__25_gs_3a__3a_ret_2d__2d__3a_168=__args.car; __args=__args.cdr;
+return new cpscm__Trampoline (function () {
+return (function (__args) {
+var cpscm_20__25_gs_3a__3a_k188=__args.car; __args=__args.cdr;
+var cpscm_5f_chk_5f_46_5f_tmp=__args.car; __args=__args.cdr;
+var cpscm_5f_pass_5f_47_5f_tmp=__args.car; __args=__args.cdr;
+return new cpscm__Trampoline (function () {
+return ((cpscm_5f_n_2d_1_5f_45 = cpscm_20__25_gs_3a__3a_ret_2d__2d__3a_168), (cpscm_5f_chk_5f_46 = cpscm_5f_chk_5f_46_5f_tmp), cpscm__reduce_2d_trampoline ((function (__args) {
+var cpscm_20__25_gs_3a__3a_ret_2d_set_21__3a_191=__args.car; __args=__args.cdr;
+return new cpscm__Trampoline (function () {
+return (cpscm_20__25_gs_3a__3a_k188) (cpscm__list (cpscm_20__25_gs_3a__3a_ret_2d_set_21__3a_191, cpscm__nil)); });
+}
+) (cpscm__list ((cpscm_5f_pass_5f_47 = cpscm_5f_pass_5f_47_5f_tmp), cpscm__nil)))); });
+}
+) (cpscm__list (function (__args) {
+var cpscm_20__25_gs_3a_167=__args.car; __args=__args.cdr;
+return new cpscm__Trampoline (function () {
+return (function (__args) {
+var cpscm_20__25_gs_3a__3a_k172=__args.car; __args=__args.cdr;
+var cpscm_5f_loop_5f_48=__args.car; __args=__args.cdr;
+return new cpscm__Trampoline (function () {
+return (function (__args) {
+var cpscm_20__25_gs_3a__3a_k175=__args.car; __args=__args.cdr;
+var cpscm_5f_loop_5f_48_5f_tmp=__args.car; __args=__args.cdr;
+return new cpscm__Trampoline (function () {
+return (function (__args) {
+var cpscm_20__25_gs_3a__3a_ret_2d_set_21__3a_176=__args.car; __args=__args.cdr;
+return new cpscm__Trampoline (function () {
+return (cpscm_20__25_gs_3a__3a_k175) (cpscm__list (cpscm_20__25_gs_3a__3a_ret_2d_set_21__3a_176, cpscm__nil)); });
+}
+) (cpscm__list ((cpscm_5f_loop_5f_48 = cpscm_5f_loop_5f_48_5f_tmp), cpscm__nil)); });
+}
+) (cpscm__list (function (__args) {
+var cpscm_20__25_gs_3a_173=__args.car; __args=__args.cdr;
+return new cpscm__Trampoline (function () {
+return (cpscm_20__25_gs_3a__3a_k172) (cpscm__list (cpscm_5f_loop_5f_48, cpscm__nil)); });
+}
+, function (__args) {
+var cpscm_20__25_gs_3a__3a_k178=__args.car; __args=__args.cdr;
+return new cpscm__Trampoline (function () {
+return (cpscm_5f_pass_5f_47) (cpscm__list (function (__args) {
+var cpscm_20__25_gs_3a__3a_ret_2d__5f_pass_5f_47_3a_181=__args.car; __args=__args.cdr;
+return new cpscm__Trampoline (function () {
+return (function (__args) {
+var cpscm_20__25_gs_3a__3a_ret_2d__20_boolean_2d__3e_combinator_3a_180=__args.car; __args=__args.cdr;
+return new cpscm__Trampoline (function () {
+return (cpscm_20__25_gs_3a__3a_ret_2d__20_boolean_2d__3e_combinator_3a_180) (cpscm__list (function (__args) {
+var cpscm_20__25_gs_3a__3a_ret_2d_apply_3a_179=__args.car; __args=__args.cdr;
+return new cpscm__Trampoline (function () {
+return (cpscm_20__25_gs_3a__3a_k178) (cpscm__list (cpscm_20__25_gs_3a__3a_ret_2d_apply_3a_179, cpscm__nil)); });
+}
+, function (__args) {
+var cpscm_20__25_gs_3a__3a_k183=__args.car; __args=__args.cdr;
+return new cpscm__Trampoline (function () {
+return (cpscm_5f_loop_5f_48) (cpscm__list (function (__args) {
+var cpscm_20__25_gs_3a__3a_ret_2d__5f_loop_5f_48_3a_184=__args.car; __args=__args.cdr;
+return new cpscm__Trampoline (function () {
+return (cpscm_20__25_gs_3a__3a_k183) (cpscm__list (cpscm_20__25_gs_3a__3a_ret_2d__5f_loop_5f_48_3a_184, cpscm__nil)); });
+}
+, cpscm__nil)); });
+}
+, function (__args) {
+var cpscm_20__25_gs_3a__3a_k186=__args.car; __args=__args.cdr;
+return new cpscm__Trampoline (function () {
+return (cpscm_20__25_gs_3a__3a_k186) (cpscm__list (cpscm__sym ("undefined"), cpscm__nil)); });
+}
+, cpscm__nil)); });
+}
+) (cpscm__list ((cpscm_20_boolean_2d__3e_combinator) (cpscm__list (cpscm_20__25_gs_3a__3a_ret_2d__5f_pass_5f_47_3a_181, cpscm__nil)), cpscm__nil)); });
+}
+, 0, false, cpscm__nil)); });
+}
+, cpscm__nil)); });
+}
+) (cpscm__list (function (__args) {
+var cpscm_20__25_gs_3a_170=__args.car; __args=__args.cdr;
+return new cpscm__Trampoline (function () {
+return (cpscm_20__25_gs_3a_170) (cpscm__list (function (__args) {
+var cpscm_20__25_gs_3a__3a_ret_2d_apply_3a_169=__args.car; __args=__args.cdr;
+return new cpscm__Trampoline (function () {
+return (cpscm_20__25_gs_3a__3a_k166) (cpscm__list (cpscm_20__25_gs_3a__3a_ret_2d_apply_3a_169, cpscm__nil)); });
+}
+, cpscm__nil)); });
+}
+, cpscm__sym ("undefined"), cpscm__nil)); });
+}
+, function (__args) {
+var cpscm_20__25_gs_3a__3a_k193=__args.car; __args=__args.cdr;
+var cpscm_5f_i_5f_48=__args.car; __args=__args.cdr;
+return new cpscm__Trampoline (function () {
+return (cpscm_5f_cref_5f_43) (cpscm__list (function (__args) {
+var cpscm_20__25_gs_3a__3a_ret_2d__5f_cref_5f_43_3a_195=__args.car; __args=__args.cdr;
+return new cpscm__Trampoline (function () {
+return (cpscm_2b_) (cpscm__list (function (__args) {
+var cpscm_20__25_gs_3a__3a_ret_2d__2b__3a_197=__args.car; __args=__args.cdr;
+return new cpscm__Trampoline (function () {
+return (cpscm_5f_cref_5f_43) (cpscm__list (function (__args) {
+var cpscm_20__25_gs_3a__3a_ret_2d__5f_cref_5f_43_3a_196=__args.car; __args=__args.cdr;
+return new cpscm__Trampoline (function () {
+return (function (__args) {
+var cpscm_20__25_gs_3a__3a_k199=__args.car; __args=__args.cdr;
+return new cpscm__Trampoline (function () {
+return (cpscm_5f_cmp_5f_42) (cpscm__list (function (__args) {
+var cpscm_20__25_gs_3a__3a_ret_2d__5f_cmp_5f_42_3a_202=__args.car; __args=__args.cdr;
+return new cpscm__Trampoline (function () {
+return (function (__args) {
+var cpscm_20__25_gs_3a__3a_ret_2d__20_boolean_2d__3e_combinator_3a_201=__args.car; __args=__args.cdr;
+return new cpscm__Trampoline (function () {
+return (cpscm_20__25_gs_3a__3a_ret_2d__20_boolean_2d__3e_combinator_3a_201) (cpscm__list (function (__args) {
+var cpscm_20__25_gs_3a__3a_ret_2d_apply_3a_200=__args.car; __args=__args.cdr;
+return new cpscm__Trampoline (function () {
+return (cpscm_20__25_gs_3a__3a_k199) (cpscm__list (cpscm_20__25_gs_3a__3a_ret_2d_apply_3a_200, cpscm__nil)); });
+}
+, function (__args) {
+var cpscm_20__25_gs_3a__3a_k204=__args.car; __args=__args.cdr;
+return new cpscm__Trampoline (function () {
+return (cpscm_20__25_gs_3a__3a_k204) (cpscm__list (false, cpscm__nil)); });
+}
+, function (__args) {
+var cpscm_20__25_gs_3a__3a_k206=__args.car; __args=__args.cdr;
+return new cpscm__Trampoline (function () {
+return (cpscm_5f_cset_21__5f_44) (cpscm__list (function (__args) {
+var cpscm_20__25_gs_3a__3a_ret_2d__5f_cset_21__5f_44_3a_207=__args.car; __args=__args.cdr;
+return new cpscm__Trampoline (function () {
+return (cpscm_2b_) (cpscm__list (function (__args) {
+var cpscm_20__25_gs_3a__3a_ret_2d__2b__3a_209=__args.car; __args=__args.cdr;
+return new cpscm__Trampoline (function () {
+return (cpscm_5f_cset_21__5f_44) (cpscm__list (function (__args) {
+var cpscm_20__25_gs_3a__3a_ret_2d__5f_cset_21__5f_44_3a_208=__args.car; __args=__args.cdr;
+return new cpscm__Trampoline (function () {
+return (cpscm_20__25_gs_3a__3a_k206) (cpscm__list (true, cpscm__nil)); });
+}
+, cpscm_20__25_gs_3a__3a_ret_2d__2b__3a_209, cpscm_20__25_gs_3a__3a_ret_2d__5f_cref_5f_43_3a_195, cpscm__nil)); });
+}
+, cpscm_5f_i_5f_48, 1, cpscm__nil)); });
+}
+, cpscm_5f_i_5f_48, cpscm_20__25_gs_3a__3a_ret_2d__5f_cref_5f_43_3a_196, cpscm__nil)); });
+}
+, cpscm__nil)); });
+}
+) (cpscm__list ((cpscm_20_boolean_2d__3e_combinator) (cpscm__list (cpscm_20__25_gs_3a__3a_ret_2d__5f_cmp_5f_42_3a_202, cpscm__nil)), cpscm__nil)); });
+}
+, cpscm_20__25_gs_3a__3a_ret_2d__5f_cref_5f_43_3a_195, cpscm_20__25_gs_3a__3a_ret_2d__5f_cref_5f_43_3a_196, cpscm__nil)); });
+}
+) (cpscm__list (function (__args) {
+var cpscm_20__25_gs_3a_194=__args.car; __args=__args.cdr;
+return new cpscm__Trampoline (function () {
+return (cpscm_20__25_gs_3a__3a_k193) (cpscm__list (cpscm_20__25_gs_3a_194, cpscm__nil)); });
+}
+, cpscm__nil)); });
+}
+, cpscm_20__25_gs_3a__3a_ret_2d__2b__3a_197, cpscm__nil)); });
+}
+, cpscm_5f_i_5f_48, 1, cpscm__nil)); });
+}
+, cpscm_5f_i_5f_48, cpscm__nil)); });
+}
+, function (__args) {
+var cpscm_20__25_gs_3a__3a_k211=__args.car; __args=__args.cdr;
+var cpscm_5f_i_5f_48=__args.car; __args=__args.cdr;
+var cpscm_5f_mod_3f__5f_49=__args.car; __args=__args.cdr;
+return new cpscm__Trampoline (function () {
+return (cpscm_3d_) (cpscm__list (function (__args) {
+var cpscm_20__25_gs_3a__3a_ret_2d__3d__3a_214=__args.car; __args=__args.cdr;
+return new cpscm__Trampoline (function () {
+return (function (__args) {
+var cpscm_20__25_gs_3a__3a_ret_2d__20_boolean_2d__3e_combinator_3a_213=__args.car; __args=__args.cdr;
+return new cpscm__Trampoline (function () {
+return (cpscm_20__25_gs_3a__3a_ret_2d__20_boolean_2d__3e_combinator_3a_213) (cpscm__list (function (__args) {
+var cpscm_20__25_gs_3a__3a_ret_2d_apply_3a_212=__args.car; __args=__args.cdr;
+return new cpscm__Trampoline (function () {
+return (cpscm_20__25_gs_3a__3a_k211) (cpscm__list (cpscm_20__25_gs_3a__3a_ret_2d_apply_3a_212, cpscm__nil)); });
+}
+, function (__args) {
+var cpscm_20__25_gs_3a__3a_k216=__args.car; __args=__args.cdr;
+return new cpscm__Trampoline (function () {
+return (cpscm_20__25_gs_3a__3a_k216) (cpscm__list (cpscm_5f_mod_3f__5f_49, cpscm__nil)); });
+}
+, function (__args) {
+var cpscm_20__25_gs_3a__3a_k218=__args.car; __args=__args.cdr;
+return new cpscm__Trampoline (function () {
+return (cpscm_2b_) (cpscm__list (function (__args) {
+var cpscm_20__25_gs_3a__3a_ret_2d__2b__3a_220=__args.car; __args=__args.cdr;
+return new cpscm__Trampoline (function () {
+return (cpscm_5f_chk_5f_46) (cpscm__list (function (__args) {
+var cpscm_20__25_gs_3a__3a_ret_2d__5f_chk_5f_46_3a_222=__args.car; __args=__args.cdr;
+return new cpscm__Trampoline (function () {
+return (function (__args) {
+var cpscm_20__25_gs_3a__3a_k224=__args.car; __args=__args.cdr;
+return new cpscm__Trampoline (function () {
+return (function (__args) {
+var cpscm_20__25_gs_3a__3a_ret_2d__20_boolean_2d__3e_combinator_3a_226=__args.car; __args=__args.cdr;
+return new cpscm__Trampoline (function () {
+return (cpscm_20__25_gs_3a__3a_ret_2d__20_boolean_2d__3e_combinator_3a_226) (cpscm__list (function (__args) {
+var cpscm_20__25_gs_3a__3a_ret_2d_apply_3a_225=__args.car; __args=__args.cdr;
+return new cpscm__Trampoline (function () {
+return (cpscm_20__25_gs_3a__3a_k224) (cpscm__list (cpscm_20__25_gs_3a__3a_ret_2d_apply_3a_225, cpscm__nil)); });
+}
+, function (__args) {
+var cpscm_20__25_gs_3a__3a_k228=__args.car; __args=__args.cdr;
+return new cpscm__Trampoline (function () {
+return (cpscm_20__25_gs_3a__3a_k228) (cpscm__list (cpscm_20__25_gs_3a__3a_ret_2d__5f_chk_5f_46_3a_222, cpscm__nil)); });
+}
+, function (__args) {
+var cpscm_20__25_gs_3a__3a_k230=__args.car; __args=__args.cdr;
+return new cpscm__Trampoline (function () {
+return (cpscm_20__25_gs_3a__3a_k230) (cpscm__list (cpscm_5f_mod_3f__5f_49, cpscm__nil)); });
+}
+, cpscm__nil)); });
+}
+) (cpscm__list ((cpscm_20_boolean_2d__3e_combinator) (cpscm__list (cpscm_20__25_gs_3a__3a_ret_2d__5f_chk_5f_46_3a_222, cpscm__nil)), cpscm__nil)); });
+}
+) (cpscm__list (function (__args) {
+var cpscm_20__25_gs_3a_221=__args.car; __args=__args.cdr;
+return new cpscm__Trampoline (function () {
+return (cpscm_5f_pass_5f_47) (cpscm__list (function (__args) {
+var cpscm_20__25_gs_3a__3a_ret_2d__5f_pass_5f_47_3a_219=__args.car; __args=__args.cdr;
+return new cpscm__Trampoline (function () {
+return (cpscm_20__25_gs_3a__3a_k218) (cpscm__list (cpscm_20__25_gs_3a__3a_ret_2d__5f_pass_5f_47_3a_219, cpscm__nil)); });
+}
+, cpscm_20__25_gs_3a__3a_ret_2d__2b__3a_220, cpscm_20__25_gs_3a_221, cpscm__nil)); });
+}
+, cpscm__nil)); });
+}
+, cpscm_5f_i_5f_48, cpscm__nil)); });
+}
+, cpscm_5f_i_5f_48, 1, cpscm__nil)); });
+}
+, cpscm__nil)); });
+}
+) (cpscm__list ((cpscm_20_boolean_2d__3e_combinator) (cpscm__list (cpscm_20__25_gs_3a__3a_ret_2d__3d__3a_214, cpscm__nil)), cpscm__nil)); });
+}
+, cpscm_5f_i_5f_48, cpscm_5f_n_2d_1_5f_45, cpscm__nil)); });
+}
+, cpscm__nil)); });
+}
+, cpscm_5f_n_5f_41, 1, cpscm__nil)); });
+}
+) (cpscm__list (function (__args) {
+var cpscm_20__25_gs_3a_164=__args.car; __args=__args.cdr;
+return new cpscm__Trampoline (function () {
+return (cpscm_20__25_gs_3a__3a_k163) (cpscm__list (cpscm_20__25_gs_3a_164, cpscm__nil)); });
+}
+, cpscm__sym ("undefined"), cpscm__sym ("undefined"), cpscm__sym ("undefined"), cpscm__nil)); });
+}
+;
+
+
